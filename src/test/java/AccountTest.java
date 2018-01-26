22b4293757fffad416ddbe4554f6ca8dace65e72
Anastasiia Shulichenko <nastusenka@gmail.com>
2018-01-26 16:46:22 +0100

    Add deposit metod test

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,5 +1,6 @@
 import com.nastusenka.bankingkata.Account;
 import com.nastusenka.bankingkata.exceptions.InvalidBalanceException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AccountTest {
@@ -7,5 +8,12 @@ public class AccountTest {
     @Test(expected = InvalidBalanceException.class)
     public void testInputBalanceWrong() throws InvalidBalanceException {
         new Account(-1);
+    }
+
+    @Test
+    public void testDeposit() {
+        Account account = new Account(50);
+        account.deposit(500);
+        Assert.assertEquals(550; account.getAccountBalance());
     }
 }
