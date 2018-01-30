0062ee262ca9d5351f097b542271a3bc9bb716ed
Anastasiia Shulichenko <nastusenka@gmail.com>
2018-01-30 23:31:10 +0100

    Add Javadoc

--- src/main/java/com/nastusenka/bankingkata/Account.java
+++ src/main/java/com/nastusenka/bankingkata/Account.java
@@ -12,6 +12,12 @@ public class Account {
     private int accountBalance;
     private List<Transaction> transactions = new ArrayList<>();
 
+    /**
+     * Constructor with parameters validation
+     *
+     * @param accountBalance Initial amount for account creation
+     * @throws InvalidBalanceException when the balance is below zero
+     */
     public Account(int accountBalance) throws InvalidBalanceException {
         if (accountBalance < 0) throw new InvalidBalanceException();
         this.accountBalance = accountBalance;
@@ -21,16 +27,28 @@ public class Account {
         return accountBalance;
     }
 
+    /**
+     * Replenish account and save transaction
+     *
+     * @param accountAmount Amount to deposit
+     */
     public void deposit(int accountAmount) {
-        if (accountAmount < 0) { throw new IllegalArgumentException("The parameter cann't be below zero");}
+        if (accountAmount < 0) { throw new IllegalArgumentException("The parameter can't be below zero");}
         accountBalance = accountBalance + accountAmount;
         LocalDateTime dateTime = null;
         Transaction transaction = new Transaction(accountBalance, accountAmount, dateTime.now());
         transactions.add(transaction);
     }
 
+    /**
+     * Withdraw from account and save transaction
+     *
+     * @param accountAmount Amount to withdraw
+     * @throws InvalidBalanceException when the balance is below zero
+     * @throws IllegalArgumentException when the parameter is below zero
+     */
     public void withdraw(int accountAmount) throws InvalidBalanceException, IllegalArgumentException {
-        if (accountAmount < 0) { throw new IllegalArgumentException("The parameter cann't be below zero");}
+        if (accountAmount < 0) { throw new IllegalArgumentException("The parameter can't be below zero");}
         if (accountAmount > accountBalance) {
             throw new InvalidBalanceException();
         }
@@ -40,6 +58,11 @@ public class Account {
         transactions.add(transaction);
     }
 
+    /**
+     * Print saved transactions
+     *
+     * @return Formated statement
+     */
     public String printStatement() {
         String output = String.format("|%20s|%20s|%20s|%n", "DATE", "AMOUNT", "BALANCE");
         for (Transaction temp : transactions) {
